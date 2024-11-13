c03cdc4afc890c4eb19eae006a803327ad18b028
yoranvoorter <yoran@yoranvoorter.be>
2024-11-13 10:42:25 +0100

    Mastermind-01, Basic layout using comboboxes and ellipses

---
+++ MasterMindWPL/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MasterMindWPL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Color[] _availableColors = { Colors.Red, Colors.Yellow, Colors.White, Colors.Green, Colors.Blue };
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void AddColorsToComboBoxes()
+        {
+            foreach (Color color in _availableColors)
+            {
+                cboColors1.Items.Add(color);
+                cboColors2.Items.Add(color);
+                cboColors3.Items.Add(color);
+                cboColors4.Items.Add(color);
+            }
+        }
+    }
+}
